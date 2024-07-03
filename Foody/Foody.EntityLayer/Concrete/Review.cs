59550e56a4ff620d759397cd1d0330052ec127d3
1kivanc <kivanc0556@gmail.com>
2024-07-03 11:51:41 +0300

    entities are completed

---
+++ Foody/Foody.EntityLayer/Concrete/Review.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foody.EntityLayer.Concrete
+{
+    public class Review
+    {
+        public int ReviewId { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerTitle { get; set; }
+        public string Comment { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
