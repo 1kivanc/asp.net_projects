8414db249303693ae2d86e4a88a9bd04b0c3baae
1kivanc <kivanc0556@gmail.com>
2024-07-03 17:09:40 +0300

    Add generic repository interface (IGenericDal)

---
+++ Foody/Foody.DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foody.DataAccessLayer.Abstract
+{
+    public interface IGenericDal<T> where T : class
+    {
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(int id);
+        List<T> GetAll();
+        T GetById(int id);
+
+    }
+}
