427af53715802a469a2b7f29110f236cd3a915f4
1kivanc <kivanc0556@gmail.com>
2024-07-12 11:00:03 +0300

    Repository Design Pattern

---
+++ Foody/Foody.DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,49 @@
+﻿using Foody.DataAccessLayer.Abstract;
+using Foody.DataAccessLayer.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foody.DataAccessLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        private readonly FoodyContext _context;
+
+        public GenericRepository(FoodyContext context)
+        {
+            _context = context;
+        }
+
+        public void Delete(int id)
+        {
+            var value = _context.Set<T>().Find(id);
+            _context.Set<T>().Remove(value);
+            _context.SaveChanges();
+        }
+
+        public List<T> GetAll()
+        {
+            return _context.Set<T>().ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return _context.Set<T>().Find(id);
+        }
+
+        public void Insert(T entity)
+        {
+            _context.Set<T>().Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            _context.Set<T>().Update(entity);
+            _context.SaveChanges();
+        }
+    }
+}
