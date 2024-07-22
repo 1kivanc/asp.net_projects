29fb817322ef15be5d66d9e99b47cd5152095f11
1kivanc <kivanc0556@gmail.com>
2024-07-22 21:56:42 +0300

    layout page

---
+++ Foody/Foody.PresentationLayer/ViewComponents/AdminLayoutViewComponents/_HeadLayoutComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Foody.PresentationLayer.ViewComponents.AdminLayoutViewComponents
+{
+	public class _HeadLayoutComponentPartial:ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+
+	}
+}
