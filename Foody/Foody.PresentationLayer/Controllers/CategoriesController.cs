6bdccee69c4207029cac71553d7f4e06b888804d
1kivanc <kivanc0556@gmail.com>
2024-07-23 23:10:37 +0300

    Category page table preparation

---
+++ Foody/Foody.PresentationLayer/Controllers/CategoriesController.cs
@@ -0,0 +1,20 @@
+﻿using Foody.BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Foody.PresentationLayer.Controllers
+{
+	public class CategoriesController : Controller
+	{
+		private readonly ICategoryService _categoryService;
+
+		public CategoriesController(ICategoryService categoryService)
+		{
+			_categoryService = categoryService;
+		}
+		public IActionResult CategoryList()
+		{
+			var values = _categoryService.TGetAll();
+			return View(values);
+		}
+	}
+}
